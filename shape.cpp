d54924a17bf2f7ba4d16c99977222afe38b00c30
KyinE1 <83590743+KyinE1@users.noreply.github.com>
2021-12-21 09:07:05 -0300

    Update shape.cpp

--- shape.cpp
+++ shape.cpp
@@ -1,20 +1,14 @@
-/*
-	shape.cpp
-	Date Written: 4/22
-	Created by: Kyin Edwards
-*/
-
 #include "shape.h"
 
-/* 
-	Constructor 
-	@param color The shape's color.
-*/
+/** Constructor. 
+ *	
+ *	@param color: The shape's color.
+ */
 shape::shape(const std::string& color) : color(color) {
 	// this->color = color would be the equivalent in the list
 }
 
-/* Print the color of the shape. */
+/** Print the color of the shape. */
 void shape::print() const {
 	std::cout << color; 
 }

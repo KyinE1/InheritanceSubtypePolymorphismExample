1127e396f9eabf90a06fb0acdc8e52d0fafe2bbd
KyinE1 <83590743+KyinE1@users.noreply.github.com>
2021-12-21 09:06:25 -0300

    Update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -1,30 +1,23 @@
-/*
-	triangle.cpp
-	Date Written: 4/22
-	Created by Kyin Edwards
-*/
-
 #include "triangle.h"
 
-/* 
-	Constructor
-	@param color The triangle's color.
-	@param height The triangle's height.
-	@param base The triangle's base.
-*/
+/** Constructor.
+ *	@param color: The triangle's color.
+ *	@param height: The triangle's height.
+ *	@param base: The triangle's base.
+ */
 triangle::triangle(const std::string& color, int height, int base) : shape(color), height(height), base(base) {
 	// this->height = height in list
 	// this->base = base in list
 	// shape(color) passed to the shape function (shape.h)
 }
 
-/* @return Overloaded print method to call base class print method. */
+/** Overloaded print method to call base class method. */
 void triangle::print() const {
 	shape::print();
 	std::cout << " triangle, height " << height << ", base " << base << ", area " << get_area() << "\n";
 }
 
-/* @return Overloaded get_area method to compute shape's area. */
+/** Accessor method. */
 double triangle::get_area() const {
 	return (.5) * height * base;
 } 

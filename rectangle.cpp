c5baad94596025d5f7c78e4edea1c72ef56d495a
KyinE1 <83590743+KyinE1@users.noreply.github.com>
2021-12-21 09:08:06 -0300

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,30 +1,23 @@
-/*
-	rectangle.cpp
-	Date Written: 4/22
-	Created by Kyin Edwards
-*/
-
 #include "rectangle.h"
 
-/* 
-	Constructor
-	@param color The rectangle's color.
-	@param height The rectangle's height.
-	@param width The rectangle's width.
-*/
+/** Constructor.
+ *	@param color: The rectangle's color.
+ *	@param height: The rectangle's height.
+ *	@param width: The rectangle's width.
+ */
 rectangle::rectangle(const std::string& color, int height, int width) : shape(color), height(height), width(width) {
 	// this->height = height in list
 	// this->width = width in list
 	// shape(color) passed to the shape function (shape.h)
 }
 
-/* Overloaded print method to call base class print method. */
+/** Overloaded print method to call base class method. */
 void rectangle::print() const {
 	shape::print();
 	std::cout << " rectangle, height " << height << ", width " << width << ", area " << get_area() << "\n";
 }
 
-/* @return Overloaded get_area method to compute shape's area. */
+/** Accessor method. */
 double rectangle::get_area() const {
 	return height * width;
 } 
